f63772b624d5d9c5609ca5d9bb11e36709ec7979
sherzodkenjaev <kenjaevsherzod@gmail.com>
2021-08-22 16:03:23 +0500

    change in about

--- src/components/Introme.jsx
+++ src/components/Introme.jsx
@@ -10,14 +10,14 @@ function Introme() {
           <h1>Nice to meet you</h1>
           <br />
           <p>
-            I’m passionate web developer. I love developing productive and user
-            friendly products. I put my heart into my work and I use my maximal
-            capabilities to complete tasks on hand. My thirst for knowledge and
-            new skills help me to be up to date with industry standards. My
-            interests are finding jobs with future career prospects, looking
-            forward to get high experience by working with specialists and the
-            ability to realize own potential and to be as helpful as possible to
-            the overall team.
+            I'm a self-taught frontend web developer. My interests are finding
+            job with future career prospects, looking forward to get high
+            experience by working with specialists and the ability to realize
+            own potential and to be as helpful as possible to the overall team.
+            I love developing productive and user friendly products. I use my
+            maximal capabilities to complete tasks on hand. My thirst for
+            knowledge and new skills help me to be up to date with industry
+            standards.
           </p>
         </Fade>
       </div>
